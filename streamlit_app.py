781b2d8e6611a5363fb89efe3c4adeef032cd990
Neojh011 <135089388+Neojh011@users.noreply.github.com>
2023-05-31 11:38:40 +0800

    Update streamlit_app.py
    Added new DIY smoothie header

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,4 +8,5 @@ streamlit.text('🥗 Kale, Spinach and Rocket Smoothie')
 streamlit.text('🐔 Hard-boiled Free-range eggs')
 streamlit.text('🥑🍞 Avacado Toast')
 
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
