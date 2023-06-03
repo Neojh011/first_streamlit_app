5cf72c753269364c61ed8b58670b953eff9532d7
Neojh011 <135089388+Neojh011@users.noreply.github.com>
2023-06-03 22:11:45 +0800

    Update streamlit_app.py
    added insert statement

--- streamlit_app.py
+++ streamlit_app.py
@@ -35,12 +35,14 @@ streamlit.dataframe(fruityvice_normalized)
 
 import snowflake.connector
 
-add_my_fruit = streamlit.text_input('What fruit would you to add?','jackfruit')
-streamlit.write('Thanks for adding ', add_my_fruit)
-
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("select * from fruit_load_list")
 my_data_rows = my_cur.fetchall()
 streamlit.header("The fruit list contains:")
 streamlit.dataframe(my_data_rows)
+
+add_my_fruit = streamlit.text_input('What fruit would you to add?','jackfruit')
+streamlit.write('Thanks for adding ', add_my_fruit)
+
+my_cur.execute("insert into fruit_load_list values ('from streamlit')")
